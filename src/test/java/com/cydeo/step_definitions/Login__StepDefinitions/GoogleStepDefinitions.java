dc66e6bcead1c2b7bc9b3053abd47bc7a5a51ce1
pakdil <eaasbb@gmail.com>
2022-10-12 08:11:55 +0300

    BrowserUtils

---
+++ src/test/java/com/cydeo/step_definitions/Login__StepDefinitions/GoogleStepDefinitions.java
@@ -0,0 +1,23 @@
+package com.cydeo.step_definitions.Login__StepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.sql.Driver;
+
+public class GoogleStepDefinitions {
+
+    @When("user is on Google search page")
+    public void user_is_on_google_search_page() {
+        
+
+    }
+    @Then("user should see title is Google")
+    public void user_should_see_title_is_google() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+}
--- src/test/java/com/cydeo/step_definitions/Login__StepDefinitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login__StepDefinitions/Login_StepDefinitions.java
@@ -12,11 +12,10 @@ public class Login_StepDefinitions {
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
         System.out.println("user enters librarian password");
-
     }
-
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
         System.out.println("user should see the dashboard");
+
     }
 }
