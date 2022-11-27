8c3037c7792975218d341e468e6abbeb248e3627
pakdil <eaasbb@gmail.com>
2022-11-27 15:16:35 +0300

    day 16 Hooks.java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -6,10 +6,9 @@ each scenario and each step
  */
 
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -21,7 +20,16 @@ public class Hooks {
     }
 
     @After
-    public void teardownScenario() {
+    public void teardownScenario(Scenario scenario) {
+
+        //scenario.isFailed() --> if scenario fails this method will return True boolean value
+if (scenario.isFailed()) {
+
+
+    byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+    scenario.attach(screenshot, "image/png", scenario.getName());
+
+}
 
         Driver.closeDriver();
 
