cabeb1058bd3596a670452c87f7dd732f41062e1
pakdil <eaasbb@gmail.com>
2022-12-09 22:26:15 +0300

    day 17 WebTable_StepDefinitions

---
+++ src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
@@ -0,0 +1,34 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WebTable_StepDefinitions {
+
+    @Given("user is on the login page or web table app")
+    public void user_is_on_the_login_page_or_web_table_app() {
+
+    }
+
+    @When("user enters username {string}")
+    public void userEntersUsername(String arg0) {
+    }
+
+    @And("user enters password {string}")
+    public void userEntersPassword(String arg0) {
+    }
+    @When("user clicks login button")
+    public void user_clicks_login_button() {
+
+
+    }
+    @Then("user should see url contains orders")
+    public void user_should_see_url_contains_orders() {
+
+
+    }
+
+
+}
