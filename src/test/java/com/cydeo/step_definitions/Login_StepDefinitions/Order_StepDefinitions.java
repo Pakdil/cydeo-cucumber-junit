ebe99e9c81659ab82fdee1b5ec0af47dc21690f9
pakdil <eaasbb@gmail.com>
2022-12-10 14:54:56 +0300

    day17 Order_StepDefinitions.java

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions/Order_StepDefinitions.java
@@ -4,9 +4,15 @@ import com.cydeo.pages.BasePage;
 import com.cydeo.pages.OrderPage;
 import com.cydeo.pages.ViewAllOrdersPage;
 import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 
 public class Order_StepDefinitions {
 
@@ -17,67 +23,78 @@ public class Order_StepDefinitions {
 
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        //Getting the page using the URL from configuration.properties
+        Driver.getDriver().get(ConfigurationReader.getProperty("web.table.url"));
+
+        //Calling our login method to log in to web table app
+        webTableLoginPage.login();
+
+        //clicking to "order" link to go order page
+        basePage.order.click();
     }
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Select select = new Select(orderPage.productDropdown);
+        select.selectByVisibleText(string);
     }
     @When("user enters quantity {int}")
-    public void user_enters_quantity(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_enters_quantity(Integer quantity) {
+        //accepting int argument and sending it using sendKeys() method
+        //since sendKeys() method only accepts String, we need to either concat with ""
+        //or send String.valueOf(int);
+        //orderPage.inputQuantity.sendKeys(String.valueOf(quantity));
+
+        //clear() method will delete whatever is in the input box
+        //orderPage.inputQuantity.clear();
+
+        //imitating pressing back_space button from keyboard to delete existing input
+        orderPage.inputQuantity.sendKeys(Keys.BACK_SPACE);
+
+        orderPage.inputQuantity.sendKeys(quantity+"");
     }
     @When("user enters customer name {string}")
     public void user_enters_customer_name(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.inputName.sendKeys(string);
     }
     @When("user enters street {string}")
     public void user_enters_street(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.inputStreet.sendKeys(string);
     }
     @When("user enters city {string}")
     public void user_enters_city(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.inputCity.sendKeys(string);
     }
     @When("user enters state {string}")
     public void user_enters_state(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.inputState.sendKeys(string);
     }
     @When("user enters zipcode {string}")
     public void user_enters_zipcode(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.inputZip.sendKeys(string);
     }
     @When("user selects credit card type {string}")
-    public void user_selects_credit_card_type(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_selects_credit_card_type(String expectedCardType) {
+
+        //This line will loop through the list and decide which radio button to click
+        BrowserUtils.clickRadioButton(orderPage.cardType, expectedCardType);
+
     }
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.cardNoInput.sendKeys(string);
     }
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.cardExpInput.sendKeys(string);
     }
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        orderPage.processOrderBtn.click();
     }
     @Then("user should see {string} in first row of the web table")
-    public void user_should_see_in_first_row_of_the_web_table(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_should_see_in_first_row_of_the_web_table(String expectedName) {
+        String actualName = viewAllOrdersPage.newCustomerCell.getText();
+
+        Assert.assertEquals(expectedName, actualName);
     }
 }
