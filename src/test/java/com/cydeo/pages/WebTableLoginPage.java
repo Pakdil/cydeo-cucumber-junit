c7d01ecd24ec6f9268d639042f816541ee1eb011
pakdil <eaasbb@gmail.com>
2022-12-10 12:40:03 +0300

    day17 WebTable loginMethods

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -20,5 +20,28 @@ public class WebTableLoginPage {
     @FindBy(xpath = "//button[.='Login']")
     public WebElement loginButton;
 
+    /**
+     * no parameters
+     *
+     * username: Test
+     * password: Tester
+     */
+
+    public void login() {
+        this.inputUsername.sendKeys("Test");
+        this.inputPassword.sendKeys("Tester");
+       this.loginButton.click();
+    }
+
+    /**
+     * This method will accept two arguments and login
+     * @param: username
+     * @param: password
+     */
+    public void login(String username, String password) {
+        this.inputUsername.sendKeys(username);
+        this.inputPassword.sendKeys(password);
+        this.loginButton.click();
+    }
 
 }
